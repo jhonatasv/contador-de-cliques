7ade7621c8a2faf0a036d77bf8271717eddd4577
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-01 19:10:39 -0300

    correções

--- assets/contador.js
+++ assets/contador.js
@@ -1,6 +1,5 @@
 let star = 0
 
-
 function contar() {
     total = ++ star
     document.querySelector('.text').innerHTML = `O Contador está com <mark>${star}</mark> cliques`
@@ -10,3 +9,4 @@ function zerar() {
     star = 0
     document.querySelector('.text').innerHTML = `<p>você clicou <mark>${total}</mark></p>`
 }
+
