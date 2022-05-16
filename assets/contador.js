baa11c7d2691de694603599a503f9c13b335b78b
jhonatasv <jhonatasvieira42@gmail.com>
2022-05-16 15:18:06 -0300

    add nova img

--- assets/contador.js
+++ assets/contador.js
@@ -1,6 +1,6 @@
 const contar = document.querySelector('#contar')
 const zerar = document.querySelector('#zerar')
-const conteundo = document.querySelector('#conteundo')
+const res = document.querySelector('#conteundo')
 
 
 let star = 0
@@ -8,22 +8,45 @@ let total = '';
 
 contar.addEventListener('click', function(){
     total = ++ star
-    conteundo.innerHTML = `<p> O Contador está com <mark>${star}</mark> cliques</p>`
+    res.innerHTML = `<p> O Contador está com <mark>${star}</mark> cliques</p>`
 })
 
 
 zerar.addEventListener('click', function() {
     star = 0 
+    let img = document.createElement('img')
     
     if(total >= 20 && total <= 40) {
-        let img = document.createElement('img')
         img.setAttribute('src', 'assets/img/ronald-anao.png')
         
-        conteundo.style.textAlign = 'center'
-        conteundo.innerHTML = `<p> Você clicou <mark>${total}</mark> &#x1F928</p>`
-        conteundo.appendChild(img)
+        res.style.textAlign = 'center'
+        res.innerHTML = `<p> Você clicou <mark>${total}</mark> &#x1F928</p>`
+        res.appendChild(img)
+    } else if(total >= 50 && total <= 65) {
+        img.setAttribute('src', 'assets/img/anya-assutada.png')
+       
+        res.style.textAlign = 'center'
+        res.innerHTML = `<p> Você clicou <mark>${total}</mark> &#x1F928</p>`
+        res.appendChild(img)
     } else {
-
-        conteundo.innerHTML = `<p> Você clicou <mark>${total}</mark> &#x1F928</p>`
+        res.innerHTML = `<p> Você clicou <mark>${total}</mark> &#x1F928</p>`
     }
+
 })
+
+
+// zerar.addEventListener('click', function() {
+//     star = 0 
+    
+//     if(total >= 20 && total <= 40) {
+//         let img = document.createElement('img')
+//         img.setAttribute('src', 'assets/img/ronald-anao.png')
+        
+//         conteundo.style.textAlign = 'center'
+//         conteundo.innerHTML = `<p> Você clicou <mark>${total}</mark> &#x1F928</p>`
+//         conteundo.appendChild(img)
+//     } else {
+
+//         conteundo.innerHTML = `<p> Você clicou <mark>${total}</mark> &#x1F928</p>`
+//     }
+// })
